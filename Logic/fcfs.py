bc6cb6bb244c71aba9e96ec38ba0907807042ddd
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2023-12-02 14:02:54 +0530

    Added Average Waiting and Average Turn Around Time

--- Logic/fcfs.py
+++ Logic/fcfs.py
@@ -1,20 +1,18 @@
 class Fcfs:
     def processData(self, Tasks):
         # Tasks: list of tuples representing tasks
-        # Each tuple: (process name, arrival time, burst time)
 
         # Sort tasks according to their arrival time
         Tasks = sorted(Tasks, key=lambda x: x[1])
 
         # Initialize variables
-        process_names = []
-        arrival_times = []
-        burst_times = []
         sequence = []
+        waiting_time = []
+        turnAround_time = []
         time = 0
         task_count = len(Tasks)
         indx = 0
-
+        firstFlag = True
         while indx < len(Tasks):
             # Check if current task is ready
             if Tasks[indx][2] > 0 and time >= Tasks[indx][1]:
@@ -22,26 +20,31 @@ class Fcfs:
                 sequence.append(Tasks[indx][0])
                 Tasks[indx][2] -= 1
                 time += 1
-                print(Tasks[indx][0])
+                if firstFlag:
+                    firstFlag = False
+                    waiting_time.append(time - Tasks[indx][1] - 1)
+                    turnAround_time.append(Tasks[indx][2] + waiting_time[-1] + 1)
             elif time < Tasks[indx][1]:
                 time += 1
                 sequence.append(-1)
             else:
                 indx += 1
+                firstFlag = True
                 
-            # Idle time if no task is ready
-            # if Tasks[indx][2] == 0:
-            #     sequence.append(-1)
-            #     time += 1
-            #     task_count -= 1
-
+        print("*** Waiting Time ***")
+        print(waiting_time)
+        print("****")
+        
+        print("*** Turn Around Time ***")
+        print(turnAround_time)
+        print("****")
         return sequence, time
 
 
 
 
 ff = Fcfs()
-task = list([('Task1', 2, 1, 2, -1, -1), ('Task2', 4, 1, 1, -1, -1), ('Task3', 7, 2, 3, -1, -1)])
+task = list([('Task1', 2, 12, 2, -1, -1), ('Task2', 6, 2, 1, -1, -1), ('Task3', 10, 1, 3, -1, -1)])
 ans = []
 for i in task:
     ans.append(list(i))
