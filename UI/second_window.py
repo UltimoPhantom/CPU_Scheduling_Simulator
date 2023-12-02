cd9e2d1d44e122efa1365410788161cd862548d4
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2023-12-02 18:15:25 +0530

    Updated the input window for FCFS

--- UI/second_window.py
+++ UI/second_window.py
@@ -18,8 +18,11 @@ class InputWindow2:
         self.tQ = -1
         if self.algo == 'Priority':
             self.priority_s_input(self.n)
-        else:    
+        elif self.algo == 'Round Robin':    
             self.rr_s_input(self.n)
+        elif self.algo == 'FCFS':
+            self.fcfs_input(self.n)
+            
         
     def Priority_handleSubmit(self):
         for i in range(len(self.tasks)):
@@ -239,6 +242,111 @@ class InputWindow2:
         submitButton.place(x=170, y=y)
         
         self.root.mainloop()
+    
+
+    def fcfs_input(self,n):
+        self.root.title("FCFS Scheduling")
+
+        self.main_frame = tk.Frame(self.root, width=600, height=max(600,150*n))
+        self.main_frame.pack_propagate(False)
+        self.main_frame.pack()
+
+        background_image = tk.PhotoImage(file="Assect/bg2.png")
+        background_label = tk.Label(self.main_frame, image=background_image)
+        background_label.place(relx=0, rely=0, relwidth=1, relheight=1)
+        
+        # self.tq = ctk.CTkEntry(
+        #     self.main_frame,
+        #     width=130,
+        #     height=40,
+        #     placeholder_text="Time Quantum",
+        #     corner_radius=10,
+        #     bg_color='#2e2e2e',
+        #     placeholder_text_color='#e9e084',
+        #     font=("Verdana", 12)
+        # )
+        # self.tq.place(x=160, y=10)
+        
+        x, y = 40, 40
+        for i in range(0,n):
+            placeholder = 'Task ' + str(i+1)
+            self.task = ctk.CTkEntry(
+                self.main_frame,
+                width=100,
+                height=40,
+                placeholder_text=placeholder,
+                corner_radius=10,
+                bg_color='#2e2e2e',
+                placeholder_text_color='#e9e084',
+                font=("Verdana", 12)
+            )
+            self.task.place(x=x, y=y)
+            self.tasks.append(self.task)
+            x += 120
+            
+            
+            placeholder = 'Arrival Time ' + str(i+1)
+            
+            self.arrivalTime = ctk.CTkEntry(
+                self.main_frame,
+                width=120,
+                height=40,
+                placeholder_text=placeholder,
+                corner_radius=10,
+                bg_color='#2e2e2e',
+                placeholder_text_color='#e9e084',
+                font=("Verdana", 12)
+            )
+            self.arrivalTime.place(x=x, y=y)
+            self.at.append(self.arrivalTime)
+            x += 140
+            
+            placeholder = 'Burst Time ' + str(i+1)
+            self.burstTime = ctk.CTkEntry(
+                self.main_frame,
+                width=100,
+                height=40,
+                placeholder_text=placeholder,
+                corner_radius=10,
+                bg_color='#2e2e2e',
+                placeholder_text_color='#e9e084',
+                font=("Verdana", 12)
+            )
+            self.burstTime.place(x=x, y=y)
+            self.bt.append(self.burstTime)
+            x += 140
+
+            y += 80
+            x = 40
+
+        submitButton = ctk.CTkButton(
+            self.main_frame,
+            text='Submit',
+            corner_radius=25,
+            fg_color='#e9e084',
+            hover_color='#9c9658',
+            border_color='purple',
+            text_color='black',
+            bg_color='#1f1f1f',
+            width=100,
+            height=35,
+            command=self.fcfs_handleSubmit
+        )
+        submitButton.place(x=170, y=y)
+        
+        self.root.mainloop()
+        
+    def fcfs_handleSubmit(self):
+        for i in range(len(self.tasks)):
+            self.task_info.append((
+                self.tasks[i].get(),
+                int(self.at[i].get()),
+                int(self.bt[i].get()),
+                -1,  #Priority set to -1 in RR algo
+                -1,  #Execution time initialized to -1
+                -1   #Completion time initialized to -1
+            ))
+        self.root.destroy()
     
 
     def get_input_values(self):
--- main.py
+++ main.py
@@ -27,11 +27,11 @@
 
 from UI.first_window import InputWindow
 from UI.second_window import InputWindow2
+from UI.Priority_Output import Priority_Output
+from UI.fcfs_window import FCFS_Output
 from Data.insertion import insert_values
 from Logic.priority_prem import Priority
-from UI.Priority_Output import Priority_Output
 from Logic.fcfs import Fcfs
-from UI.fcfs_window import FCFS_Output
 
 input_window = InputWindow()
 ans = input_window.get_input_values()
@@ -53,11 +53,10 @@ if ans['algo'] == "Priority":
     # priority_instance_output.start_updates()  # Start updates in Priority_Output
     priority_instance_output.mainloop()
 
-if ans['algo'] == 'FCFS':
+elif ans['algo'] == 'FCFS':
     Fcfs_instance = Fcfs()
     res = Fcfs_instance.processData(Tasks)
     print("RES RES  ",res)
     print(Tasks, res)
-    
     fcfs_instance_output = FCFS_Output(Tasks, res)
     fcfs_instance_output.mainloop()
